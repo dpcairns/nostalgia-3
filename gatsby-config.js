6e5760f2ce77a17f48894993eba82be24115f8c7
dpcairns <dpcairns@gmail.com>
2018-09-06 00:27:13 -0700

    premove protocol

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
         resolve: `gatsby-source-wordpress`,
         options: {
           baseUrl: `46.101.187.207`,
-          protocol: `http`,
           hostingWPCOM: false,
           useACF: true,
           verboseOutput: true
