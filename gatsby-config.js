0e912ff5c6e7eb85379bb3a4fd3efc821e17cd8f
dpcairns <dpcairns@gmail.com>
2018-09-05 21:57:45 -0700

    add protocol

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
         resolve: `gatsby-source-wordpress`,
         options: {
           baseUrl: `46.101.187.207`,
+          protocol: `http`,
           hostingWPCOM: false,
           useACF: true,
       },
